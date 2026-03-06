0cf16af081dcbfc932c282b6fe4261ec11c3538a
Joel Badege <jbadege1@nait.ca>
2026-03-05 23:32:19 -0700

    added Student, Course and Enrollment entity classes

---
+++ sdev2301_a1_JoelBadege/sdev2301_a1_JoelBadege/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sdev2301_a1_JoelBadege.Models
+{
+    public class Course
+    {
+        public int Id { get; private set; }
+        public string Code { get; set; } = "";
+        public string Name { get; set; } = "";
+        public int Credits { get; set; }
+
+        public List<Enrollment> Enrollments { get; set; } = new();
+    }
+}
---
+++ sdev2301_a1_JoelBadege/sdev2301_a1_JoelBadege/Models/Enrollment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sdev2301_a1_JoelBadege.Models
+{
+    public class Enrollment
+    {
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+
+        public Student Student { get; set; } = null!;
+        public Course Course { get; set; } = null!;
+    }
+}
