1bd3dc7db05d6b8068cb24ac3719b72e030af486
Joel Badege <jbadege1@nait.ca>
2026-03-06 00:11:11 -0700

    AppDbContext with SQLite entities and relationships

---
+++ sdev2301_a1_JoelBadege/sdev2301_a1_JoelBadege/Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sdev2301_a1_JoelBadege.Models;
+
+namespace sdev2301_a1_JoelBadgee.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+            modelBuilder.Entity<Enrollment>()
+                .HasKey(e => new { e.StudentId, e.CourseId });
+
+            
+            modelBuilder.Entity<Course>()
+                .HasIndex(c => c.Code)
+                .IsUnique();
+
+            
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Student)
+                .WithMany(s => s.Enrollments)
+                .HasForeignKey(e => e.StudentId);
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Course)
+                .WithMany(c => c.Enrollments)
+                .HasForeignKey(e => e.CourseId);
+        }
+    }
+}
